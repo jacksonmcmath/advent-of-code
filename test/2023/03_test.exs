ff4151fc12e12e4b5f88efe11f9166e1b00e1094
Jackson McMath <jackson.mcmath@cfacorp.com>
2023-12-30 22:37:18 -0500

    Add solutions for 2023 day 03

---
+++ test/2023/03_test.exs
@@ -0,0 +1,46 @@
+defmodule AdventOfCode.Y2023.D03Test do
+  @moduledoc false
+  use ExUnit.Case
+
+  alias AdventOfCode.Y2023.D03, as: Solution
+
+  @moduletag year: 2023, day: 3
+
+  test "Year 2023, Day 03, part_1/1" do
+    input = """
+    467..114..
+    ...*......
+    ..35..633.
+    ......#...
+    617*......
+    .....+.58.
+    ..592.....
+    ......755.
+    ...$.*....
+    .664.598..
+    """
+
+    assert Solution.part_1(input) == 4361
+  end
+
+  test "Year 2023, Day 03, part_2/1" do
+    input = """
+    467..114..
+    ...*......
+    ..35..633.
+    ......#...
+    617*......
+    .....+.58.
+    ..592.....
+    ......755.
+    ...$.*....
+    .664.598..
+    """
+
+    assert Solution.part_2(input) == 467_835
+  end
+
+  test "Year 2023, Day 03, run/0" do
+    assert Solution.run() == {536_576, 75_741_499}
+  end
+end
