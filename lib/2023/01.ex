158ab051a9ed357fdcf0aec83e58243d41d49006
Jackson McMath <jackson.mcmath@cfacorp.com>
2023-12-30 22:18:48 -0500

    Add solutions for 2023 day 01

---
+++ lib/2023/01.ex
@@ -0,0 +1,49 @@
+defmodule AdventOfCode.Y2023.D01 do
+  @moduledoc """
+  --- Day 1: Trebuchet?! ---
+  Puzzle Link: https://adventofcode.com/2023/day/1
+  """
+  @behaviour AdventOfCode.Solution
+
+  defp input(), do: AdventOfCode.Input.get!(2023, 1)
+
+  @impl AdventOfCode.Solution
+  def run(), do: {part_1(input()), part_2(input())}
+
+  @impl AdventOfCode.Solution
+  def part_1(input) do
+    input
+    |> String.replace(~r"[^\d\n]", "")
+    |> String.split("\n")
+    |> Enum.map(&extract_calibration_value/1)
+    |> Enum.sum()
+  end
+
+  @impl AdventOfCode.Solution
+  def part_2(input) do
+    input
+    |> replace_words_with_numbers()
+    |> part_1()
+  end
+
+  defp extract_calibration_value(""), do: 0
+
+  defp extract_calibration_value(input) do
+    first = String.first(input)
+    last = String.last(input)
+    String.to_integer(first <> last)
+  end
+
+  defp replace_words_with_numbers(text) do
+    text
+    |> String.replace("one", "o1e")
+    |> String.replace("two", "t2o")
+    |> String.replace("three", "t3e")
+    |> String.replace("four", "f4r")
+    |> String.replace("five", "f5e")
+    |> String.replace("six", "s6x")
+    |> String.replace("seven", "s7n")
+    |> String.replace("eight", "e8t")
+    |> String.replace("nine", "n9e")
+  end
+end
---
+++ test/2023/01_test.exs
@@ -0,0 +1,37 @@
+defmodule AdventOfCode.Y2023.D01Test do
+  @moduledoc false
+  use ExUnit.Case
+
+  alias AdventOfCode.Y2023.D01, as: Solution
+
+  @moduletag year: 2023, day: 1
+
+  test "Year 2023, Day 01, part_1/1" do
+    input = """
+    1abc2
+    pqr3stu8vwx
+    a1b2c3d4e5f
+    treb7uchet
+    """
+
+    assert Solution.part_1(input) == 142
+  end
+
+  test "Year 2023, Day 01, part_2/1" do
+    input = """
+    two1nine
+    eightwothree
+    abcone2threexyz
+    xtwone3four
+    4nineeightseven2
+    zoneight234
+    7pqrstsixteen
+    """
+
+    assert Solution.part_2(input) == 281
+  end
+
+  test "Year 2023, Day 01, run/0" do
+    assert Solution.run() == {54_601, 54_078}
+  end
+end
