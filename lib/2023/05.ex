8398b6d37a41230a82bde492b04c5952b2b9d83b
Jackson McMath <jackson.mcmath@cfacorp.com>
2023-12-30 22:54:47 -0500

    Add solutions for 2023 day 05

---
+++ lib/2023/05.ex
@@ -0,0 +1,136 @@
+defmodule AdventOfCode.Y2023.D05 do
+  @moduledoc """
+  --- Day 5: If You Give A Seed A Fertilizer ---
+  Puzzle Link: https://adventofcode.com/2023/day/5
+  """
+  @behaviour AdventOfCode.Solution
+
+  defp input(), do: AdventOfCode.Input.get!(2023, 5)
+
+  @impl AdventOfCode.Solution
+  def run(), do: {part_1(input()), part_2(input())}
+
+  @impl AdventOfCode.Solution
+  def part_1(input) do
+    {seeds, mappings} = parse_input(input)
+
+    seeds
+    |> Enum.map(fn seed -> Enum.reduce(mappings, seed, &get_destination/2) end)
+    |> Enum.min()
+  end
+
+  @impl AdventOfCode.Solution
+  def part_2(input) do
+    {seeds, mappings} = parse_input(input)
+
+    mappings
+    |> Enum.reduce(parse_seeds_as_ranges(seeds), fn mapping, sources ->
+      Enum.flat_map(sources, &get_destinations(mapping, &1))
+    end)
+    |> Enum.min()
+    |> elem(0)
+  end
+
+  defp parse_input(input) do
+    ["seeds: " <> seeds | maps] = String.split(input, "\n\n", trim: true)
+
+    {
+      seeds |> String.split(" ", trim: true) |> Enum.map(&String.to_integer/1),
+      maps |> parse_mappings()
+    }
+  end
+
+  defp parse_seeds_as_ranges(seeds) do
+    seeds
+    |> Enum.chunk_every(2)
+    |> Enum.map(fn [start, length] -> {start, start + length - 1} end)
+  end
+
+  defp parse_mappings(mappings) do
+    for mapping <- mappings do
+      with [_, ranges] <- String.split(mapping, " map:\n", trim: true) do
+        ranges
+        |> String.split("\n", trim: true)
+        |> Enum.map(fn range ->
+          range
+          |> String.split(" ", trim: true)
+          |> Enum.map(&String.to_integer/1)
+        end)
+        |> Enum.reduce(:gb_trees.empty(), fn [dst, src, len], tree ->
+          :gb_trees.insert(
+            {src, src + len - 1},
+            {dst, dst + len - 1},
+            tree
+          )
+        end)
+      end
+    end
+  end
+
+  defp get_destination(mapping, source) do
+    mapping
+    |> :gb_trees.iterator()
+    |> Stream.unfold(fn iterator ->
+      case :gb_trees.next(iterator) do
+        :none ->
+          nil
+
+        {source_range, destination_range, iterator} ->
+          {{source_range, destination_range}, iterator}
+      end
+    end)
+    |> Enum.find({{0, 0}, {0, 0}}, fn {{a, b}, _} -> source in a..b end)
+    |> then(fn {{a, _}, {c, _}} -> c + source - a end)
+  end
+
+  defp get_destinations(mapping, range) do
+    mapping
+    |> :gb_trees.iterator()
+    |> get_destinations(range, [])
+  end
+
+  defp get_destinations(iterator, {a, b}, acc),
+    do: build_destinations(:gb_trees.next(iterator), {a, b}, acc)
+
+  # ¯\_(ツ)_/¯
+  defp build_destinations(:none, {a, b}, acc), do: [{a, b} | acc]
+
+  #            a -- b
+  # si -- sf
+  defp build_destinations({{_, sf}, {_, _}, iter}, {a, b}, acc) when sf < a,
+    do: get_destinations(iter, {a, b}, acc)
+
+  # a -- b
+  #         si -- sf
+  defp build_destinations({{si, _}, {_, _}, _}, {a, b}, acc) when si > b, do: [{a, b} | acc]
+
+  #      a -- b
+  #  si -------- sf
+  defp build_destinations({{si, sf}, {di, _}, _}, {a, b}, acc) when si <= a and sf >= b,
+    do: [{a - si + di, b - si + di} | acc]
+
+  #  a ---------- b
+  #  si -- sf
+  defp build_destinations({{si, sf}, {di, df}, iter}, {a, b}, acc) when si == a and sf < b,
+    do: get_destinations(iter, {sf + 1, b}, [{di, df} | acc])
+
+  # a ------- b
+  #    si -- sf
+  defp build_destinations({{si, sf}, {di, df}, _}, {a, b}, acc) when si > a and sf == b,
+    do: [{a, si - 1}, {di, df} | acc]
+
+  # a ------------ b
+  #    si -- sf
+  defp build_destinations({{si, sf}, {di, df}, iter}, {a, b}, acc) when si > a and sf < b,
+    do: get_destinations(iter, {sf + 1, b}, [{a, si - 1}, {di, df} | acc])
+
+  #      a ----- b
+  # si ---- sf
+  defp build_destinations({{si, sf}, {di, df}, iter}, {a, b}, acc) when si < a and sf < b,
+    do: get_destinations(iter, {sf + 1, b}, [{a - si + di, df} | acc])
+
+  # a ----- b
+  #     si ---- sf
+  defp build_destinations({{si, sf}, {_, _}, _}, {a, b}, acc) when si > a and sf > b,
+    do: [{a, b} | acc]
+end
---
+++ test/2023/05_test.exs
@@ -0,0 +1,92 @@
+defmodule AdventOfCode.Y2023.D05Test do
+  @moduledoc false
+  use ExUnit.Case
+
+  alias AdventOfCode.Y2023.D05, as: Solution
+
+  @moduletag year: 2023, day: 5
+
+  test "Year 2023, Day 05, part_1/1" do
+    input = """
+    seeds: 79 14 55 13
+
+    seed-to-soil map:
+    50 98 2
+    52 50 48
+
+    soil-to-fertilizer map:
+    0 15 37
+    37 52 2
+    39 0 15
+
+    fertilizer-to-water map:
+    49 53 8
+    0 11 42
+    42 0 7
+    57 7 4
+
+    water-to-light map:
+    88 18 7
+    18 25 70
+
+    light-to-temperature map:
+    45 77 23
+    81 45 19
+    68 64 13
+
+    temperature-to-humidity map:
+    0 69 1
+    1 0 69
+
+    humidity-to-location map:
+    60 56 37
+    56 93 4
+    """
+
+    assert Solution.part_1(input) == 35
+  end
+
+  test "Year 2023, Day 05, part_2/1" do
+    input = """
+    seeds: 79 14 55 13
+
+    seed-to-soil map:
+    50 98 2
+    52 50 48
+
+    soil-to-fertilizer map:
+    0 15 37
+    37 52 2
+    39 0 15
+
+    fertilizer-to-water map:
+    49 53 8
+    0 11 42
+    42 0 7
+    57 7 4
+
+    water-to-light map:
+    88 18 7
+    18 25 70
+
+    light-to-temperature map:
+    45 77 23
+    81 45 19
+    68 64 13
+
+    temperature-to-humidity map:
+    0 69 1
+    1 0 69
+
+    humidity-to-location map:
+    60 56 37
+    56 93 4
+    """
+
+    assert Solution.part_2(input) == 46
+  end
+
+  test "Year 2023, Day 05, run/0" do
+    assert Solution.run() == {173_706_076, 11_611_182}
+  end
+end
